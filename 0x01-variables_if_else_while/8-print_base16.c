33ced9c3beb70e7f3e8da3b57d91a091d244983d
vincent <vincenthleko95@gmail.com>
2023-09-09 05:24:21 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print hexadecimal numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char numbers = '0';
+	char letters = 'a';
+
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		letters++;
+	}
+	putchar('\n');
+	return (0);
+}
