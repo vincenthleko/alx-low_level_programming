81106bbf9bcf438bc617fc96cd8b853ff4c67113
vincent <vincenthleko95@gmail.com>
2023-09-09 04:50:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers of base ten
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int base_ten = 0;
+
+	while (base_ten <= 10)
+	{
+		putchar(base_ten);
+		base_ten++;
+	}
+	putchar('\n');
+	return (0);
+}
