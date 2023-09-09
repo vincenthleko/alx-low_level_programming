44feaf70003567d783ca2fc651f1852d52500428
vincent <vincenthleko95@gmail.com>
2023-09-09 05:07:33 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in reverse mode
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabets = 'z';
+
+	while (alphabets >= 'a')
+	{
+		putchar(alphabets);
+		alphabets--;
+	}
+	putchar('\n');
+	return (0);
+}
