7afaf282abb335fa742a8cfdc437e994df1db140
vincent <vincenthleko95@gmail.com>
2023-09-09 00:53:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,16 +11,16 @@ int main(void)
 	 c = 'a';
 	 d = 'A';
 
-while (c <= 'z')
-{
-putchar(c)
-c++;
-}
-while (d <= 'Z')
-{
-putchar(d);
-d++;
-}
-putchar ('\n');
-return (0);
+	 while (c <= 'z)
+	{
+		putchar(c);
+		c++;
+	}
+	 while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	 putchar ('\n');
+	 return (0);
 }
