cf809606e0ee9020834f3db4c02de4e77c17b563
vincent <vincenthleko95@gmail.com>
2023-09-12 13:10:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,15 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	i=0
+	i = 0;
+
 		while (i < 10)
 		{
-			for (ch = 'a'; ch <= 'z; ch++)
+			for (ch = 'a'; ch <= 'z'; ch++)
+			{
+				_putchar(ch);
+			}
+			_putchar('\n');
+			i++;
 		}
-	_putchar(ch);
-	i++
 }
