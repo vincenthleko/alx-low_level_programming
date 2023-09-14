b419bdc172493554929de25efc04859f5ad55456
vincent <vincenthleko95@gmail.com>
2023-09-14 13:24:41 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15; == 0)
+		if (i % 15 == 0)
 			printf("Fizzbuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
@@ -26,6 +26,6 @@ int main(void)
 		if (i < 100)
 			printf(" ");
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
