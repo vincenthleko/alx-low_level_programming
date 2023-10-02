242ab5979b8181ac136ab972030917551f9c2907
vincent <vincenthleko95@gmail.com>
2023-10-02 13:13:33 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,7 +8,7 @@
  * Return: 0
  */
 
-char *argstostr(int ac, char **ac)
+char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
@@ -30,7 +30,7 @@ char *argstostr(int ac, char **ac)
 	{
 		for (n = 0; av[i][n]; n++)
 		{
-			str[r] + av[i][n];
+			str[r] = av[i][n];
 			r++;
 		}
 	if (str[r] == '\0')
