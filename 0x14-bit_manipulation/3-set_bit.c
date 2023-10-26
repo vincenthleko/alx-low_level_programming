696fa6f9133bd18a18a94644dd0ad40f71b4be33
vincent <vincenthleko95@gmail.com>
2023-10-26 12:16:06 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,8 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int setbit;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1)
-			return (-1);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
 	setbit = 1 << index;
 	*n = *n | setbit;
 	return (1);
