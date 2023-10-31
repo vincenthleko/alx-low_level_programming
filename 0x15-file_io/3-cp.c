2c177ffbc1a0c138d99abe7ac8e20c09751aedcb
vincent <vincenthleko95@gmail.com>
2023-10-31 12:44:14 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void check_IO_stat(int stat, int fd, char *filename, char node);
+/**
+ * main - function copies the content of one file to another
+ * @argc: argument count
+ * @argv: argument passed
+ * Return: 1 on success, exit otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP |S_IWGRP | S_IROTH;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from fale_to\n");
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
+	ceck_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, n_read, buffer);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
+	}
+	close_src = close(src);
+	check_IO_stat(close_src, NULL, 'C');
+	close_dest = close(dest);
+	chec_IO_stat(close_dest, dest, NULL, 'C');
+	return (0);
+
+
